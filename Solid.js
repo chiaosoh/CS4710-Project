cb0b8f6b55860ffd3fba1e23c986a021e87134ae
Chiao Soh <chiao.soh@gmail.com>
2024-11-18 14:08:54 -0500

    formatting w/ prettier extension

--- Solid.js
+++ Solid.js
@@ -1,16 +1,17 @@
 class Solid {
-
-  constructor(topL,botR){
+  constructor(topL, botR) {
     var lineWidth = 1;
-    this.pos = createVector(topL.pixelPos.x-lineWidth, topL.pixelPos.y-lineWidth);
+    this.pos = createVector(
+      topL.pixelPos.x - lineWidth,
+      topL.pixelPos.y - lineWidth
+    );
     this.w = botR.pixelPos.x + tileSize - this.pos.x + lineWidth;
-    this.h =  botR.pixelPos.y + tileSize - this.pos.y + lineWidth;
+    this.h = botR.pixelPos.y + tileSize - this.pos.y + lineWidth;
     this.bottomRight = createVector(this.pos.x + this.w, this.pos.y + this.h);
-
   }
 
-
-    restrictMovement(tl, br, movement) {//player dimensions
+  restrictMovement(tl, br, movement) {
+    //player dimensions
     //add the x first
 
     var x = movement.x;
@@ -18,28 +19,43 @@ class Solid {
     //
     // movement.x = round(movement.x);
     // movement.y = round(movement.y);
-    var ptl = createVector(tl.x+movement.x, tl.y);
-    var pbr = createVector(br.x+movement.x, br.y);
+    var ptl = createVector(tl.x + movement.x, tl.y);
+    var pbr = createVector(br.x + movement.x, br.y);
 
-    if ((ptl.x <this.bottomRight.x && pbr.x > this.pos.x) &&( ptl.y < this.bottomRight.y && pbr.y > this.pos.y)) {
-
-      x=0;
+    if (
+      ptl.x < this.bottomRight.x &&
+      pbr.x > this.pos.x &&
+      ptl.y < this.bottomRight.y &&
+      pbr.y > this.pos.y
+    ) {
+      x = 0;
     }
 
     //check the y movement
-    ptl = createVector(tl.x, tl.y +movement.y);
+    ptl = createVector(tl.x, tl.y + movement.y);
     pbr = createVector(br.x, br.y + movement.y);
-    if ((ptl.x <this.bottomRight.x && pbr.x > this.pos.x) &&( ptl.y < this.bottomRight.y && pbr.y > this.pos.y)) {
-      y=0;
+    if (
+      ptl.x < this.bottomRight.x &&
+      pbr.x > this.pos.x &&
+      ptl.y < this.bottomRight.y &&
+      pbr.y > this.pos.y
+    ) {
+      y = 0;
     }
 
     return createVector(x, y);
   }
 
-  collision(ptl, pbr) {//player dimensions
+  collision(ptl, pbr) {
+    //player dimensions
     //add the x first
 
-    if ((ptl.x <this.bottomRight.x && pbr.x > this.pos.x) &&( ptl.y < this.bottomRight.y && pbr.y > this.pos.y)) {
+    if (
+      ptl.x < this.bottomRight.x &&
+      pbr.x > this.pos.x &&
+      ptl.y < this.bottomRight.y &&
+      pbr.y > this.pos.y
+    ) {
       return true;
     }
     return false;
