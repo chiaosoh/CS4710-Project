e0cf89c319fd35135e5276bf151b00ff00afa325
chiaosoh <chiao.soh@gmail.com>
2024-11-25 21:06:03 -0500

    rudimentary code for the model + integration

--- process_game_state.py
+++ process_game_state.py
@@ -1,15 +1,35 @@
 import sys
 import json
+from stable_baselines3 import PPO
+import numpy as np
+
+model = PPO.load("ppo_worlds_hardest_game.zip")
+
+def preprocess_state(data):
+    player_state = data.get("player", {})
+    # player pos
+    player_position = np.array(data.get("player", {}).get("position", [0, 0])).reshape(1, 2)
+    # dots
+    moving_obstacles = data.get("environment", {}).get("moving_obstacles", [])
+    max_dots = 5
+    dots_representation = np.array([dot['position'] for dot in moving_obstacles]).reshape(-1, 2)
+    # If there are fewer than 5 dots, pad with zeros
+    if len(dots_representation) < max_dots:
+        dots_representation = np.pad(dots_representation, ((0, max_dots - len(dots_representation)), (0, 0)), mode='constant')
+    # goal pos
+    goal_position = np.array(data.get("environment", {}).get("goal_area", {}).get("position", [0, 0])).reshape(1, 2)
+    state_representation = np.vstack([player_position, dots_representation, goal_position])
+    return state_representation
 
 def process_game_state(data):
-    player_state = data.get("player", {})
-    position = player_state.get("position", [])
-    velocity = player_state.get("velocity", [])
-    is_dead = player_state.get("is_dead", False)
-    reward = sum(position)
-    return reward, player_state
-
-
-if __name__ == "__main__":
-    input_data = sys.stdin.read()
-    process_game_state(input_data)
+    observation = preprocess_state(data)
+    action, _states = model.predict(observation, deterministic=True)
+    action = action.item()
+    action_map = {
+        0: "up-left",    1: "up",       2: "up-right",
+        3: "left",       4: "stay",     5: "right",
+        6: "down-left",  7: "down",     8: "down-right"
+    }
+    move = action_map.get(action, "unknown")
+    reward = int(sum(observation.flatten()))
+    return reward, move
---
+++ train_model.py
@@ -0,0 +1,105 @@
+import gym
+from gym import spaces
+import numpy as np
+import random
+from stable_baselines3 import PPO
+
+# Defining the env for the model to train in
+class WorldsHardestGameEnv(gym.Env):
+    def __init__(self, grid_size=10, num_dots=3):
+        super(WorldsHardestGameEnv, self).__init__()
+        
+        # Game grid size (nxn)
+        self.grid_size = grid_size
+        self.num_dots = num_dots
+        
+        # Action space, 8 directions + stay
+        self.action_space = spaces.Discrete(9)
+        
+        # Positions of cube + dots
+        self.observation_space = spaces.Box(low=0, high=self.grid_size-1, shape=(self.num_dots + 2, 2), dtype=np.int32)
+        
+        self.reset()
+    
+    def reset(self):
+        # Cube starts at center
+        self.cube_position = np.array([self.grid_size // 2, self.grid_size // 2])
+        
+        # Randomly place 3 dots (they will move randomly)
+        self.dots = np.array([[random.randint(0, self.grid_size-1), random.randint(0, self.grid_size-1)] for _ in range(self.num_dots)])
+        
+        # Randomly place the goal
+        self.goal_position = np.array([random.randint(0, self.grid_size-1), random.randint(0, self.grid_size-1)])
+        
+        return self.get_observation()
+
+    def get_observation(self):
+        return np.concatenate([self.cube_position.reshape(1, 2), self.dots, self.goal_position.reshape(1, 2)], axis=0)
+
+    def step(self, action):
+        velocity = np.array([0, 0])
+        # Tilesize / 15
+        player_speed = 50 / 15
+
+        if action == 0:  # Move up-left
+            velocity = np.array([-1, 1])
+        elif action == 1:  # Move up
+            velocity = np.array([0, 1])
+        elif action == 2:  # Move up-right
+            velocity = np.array([1, 1])
+        elif action == 3:  # Move left
+            velocity = np.array([-1, 0])
+        elif action == 5:  # Move right
+            velocity = np.array([1, 0])
+        elif action == 6:  # Move down-left
+            velocity = np.array([-1, -1])
+        elif action == 7:  # Move down
+            velocity = np.array([0, -1])
+        elif action == 8:  # Move down-right
+            velocity = np.array([1, -1])
+        elif action == 9: # Stay
+            velocity = np.array([0, 0])
+
+        velocity = velocity.astype(np.float64)
+        velocity *= player_speed
+        self.cube_position = self.cube_position.astype(np.float64)
+        self.cube_position += velocity
+
+        # Ensure the cube doesn't go out of bounds
+        self.cube_position = np.clip(self.cube_position, 0, self.grid_size-1)
+
+        # Move the dots randomly (TODO: set proper movement)
+        self.dots += np.array([random.choice([-1, 0, 1]), random.choice([-1, 0, 1])])
+        self.dots = np.clip(self.dots, 0, self.grid_size-1)
+        
+        # Dot collision check
+        done = False
+        for dot in self.dots:
+            if self.check_collision(self.cube_position, dot, player_diameter=1):
+                reward = -100  # Negative reward for collision with a dot
+                done = True
+                break
+        # Reached goal
+        if np.array_equal(self.cube_position, self.goal_position):
+            reward = 100
+            done = True
+        # Encourage cube to move towards goal
+        else:
+            distance_to_goal = np.linalg.norm(self.cube_position - self.goal_position)
+            reward = -distance_to_goal
+
+        return self.get_observation(), reward, done, {}
+    
+    def check_collision(self, player_pos, dot_pos, player_diameter=1):
+        # Distance between player and dot
+        distance = np.linalg.norm(player_pos - dot_pos)
+        if distance < player_diameter: 
+            return True
+        return False
+    
+# Train the model
+if __name__ == '__main__':
+    env = WorldsHardestGameEnv(grid_size=10, num_dots=5)
+    model = PPO("MlpPolicy", env, verbose=1)
+    model.learn(total_timesteps=10000)
+    model.save("ppo_worlds_hardest_game")
